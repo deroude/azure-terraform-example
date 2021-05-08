2abb83c9191c1e9551e9c533456b8fe5801e2fe1
deroude <rvgabriel@yahoo.com>
2021-05-08 20:32:34 +0300

    going inline, fts

--- terraform/az_get_storage_key.ps1
+++
@@ -1,5 +0,0 @@
-# Using this script we will fetch storage key which is required in terraform file to authenticate backend storage account
-
-$key=(Get-AzureRmStorageAccountKey -ResourceGroupName ${env:terraformstorager} -AccountName ${env:terraformstorageaccount}).Value[0]
-
-Write-Host "##vso[task.setvariable variable=storagekey]$key"
--- terraform/az_setup.ps1
+++
@@ -1,8 +0,0 @@
-# this will create Azure resource group
-az group create --location westeurope --name $Env:terraformstoragerg
-
-az storage account create --name $Env:terraformstorageaccount --resource-group $Env:terraformstoragerg --location westeurope --sku Standard_LRS
-
-az storage container create --name terraform --account-name $Env:terraformstorageaccount
-
-az storage account keys list -g $Env:terraformstoragerg -n $Env:terraformstorageaccount
