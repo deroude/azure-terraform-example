f596cfd91492793f6be3271dd94e9c2a4b6fb493
deroude <rvgabriel@yahoo.com>
2021-05-08 21:22:42 +0300

    storage account name

--- terraform/planteneers_azure.tf
+++ terraform/planteneers_azure.tf
@@ -57,7 +57,7 @@ resource "azurerm_app_service" "plt_be" {
 }
 
 resource "azurerm_storage_account" "plt_fe" {
-  name                     = "plt-fe-${var.environment.tag}"
+  name                     = "pltfe${var.environment.tag}"
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = azurerm_resource_group.rg.location
   account_kind             = "StorageV2"
