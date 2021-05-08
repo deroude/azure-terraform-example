2b8783f48f8d6730467ca670d2fe37676de9465d
deroude <rvgabriel@yahoo.com>
2021-05-08 20:19:16 +0300

    script fix

--- terraform/az_get_storage_key.ps1
+++ terraform/az_get_storage_key.ps1
@@ -1,5 +1,5 @@
 # Using this script we will fetch storage key which is required in terraform file to authenticate backend storage account
 
-$key=(Get-AzureRmStorageAccountKey -ResourceGroupName $Env:terraformstoragerg -AccountName $Env:terraformstorageaccount).Value[0]
+$key=(Get-AzureRmStorageAccountKey -ResourceGroupName ($Env:terraformstoragerg) -AccountName ($Env:terraformstorageaccount)).Value[0]
 
 Write-Host "##vso[task.setvariable variable=storagekey]$key"
